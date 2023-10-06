5a7f87f6e9b848175f88126cbe76fce29967dfb1
Akaten test <gxconv@gmail.com>
2023-10-06 05:55:21 +0300

    TestNG implementation

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,24 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemainNotBoundaryAmount() {
+       CashbackHackService service = new CashbackHackService();
+       int expected = 200;
+       int actual = service.remain(2800);
+       assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainBoundaryAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int actual = service.remain(2000);
+        assertEquals(actual, expected);
+    }
+}
