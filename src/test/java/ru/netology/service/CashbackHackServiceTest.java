b0448ee9c8963c7aeeda34a1f89662857855d836
Akaten test <gxconv@gmail.com>
2023-10-06 11:42:27 +0300

    Test fix

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,28 +1,26 @@
 package ru.netology.service;
 
-import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
     CashbackHackService service = new CashbackHackService();
 
-    @Test
+    @org.junit.Test
     public void testRemainWhenBelowBoundaryAmount() {
         int expected = 200;
         int actual = service.remain(800);
         assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.Test
     public void testRemainWhenBoundaryAmount() {
         int expected = 0;
         int actual = service.remain(1000);
         assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.Test
     public void testRemainWhenAboveBoundaryAmount() {
         int expected = 200;
         int actual = service.remain(2800);
