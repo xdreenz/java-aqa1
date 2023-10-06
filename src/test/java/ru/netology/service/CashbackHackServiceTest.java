bbc8af8bcc05f0eff17979b32d73209b27f628f7
Akaten test <gxconv@gmail.com>
2023-10-06 12:24:57 +0300

    JUnit4 + JUnit5

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,31 +1,50 @@
 package ru.netology.service;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
 public class CashbackHackServiceTest {
 
     CashbackHackService service = new CashbackHackService();
 
-    @Test
-    public void testRemainWhenBelowBoundaryAmount() {
+    // JUnit4 tests
+    @org.junit.Test
+    public void testJUnit4RemainWhenBelowBoundaryAmount() {
         int expected = 200;
         int actual = service.remain(800);
-        assertEquals(expected, actual);
+        org.junit.Assert.assertEquals(expected, actual);
     }
 
-    @Test
-    public void testRemainWhenBoundaryAmount() {
+    @org.junit.Test
+    public void testJUnit4RemainWhenBoundaryAmount() {
         int expected = 0;
         int actual = service.remain(1000);
-        assertEquals(expected, actual);
+        org.junit.Assert.assertEquals(expected, actual);
     }
 
-    @Test
-    public void testRemainWhenAboveBoundaryAmount() {
+    @org.junit.Test
+    public void testJUnit4RemainWhenAboveBoundaryAmount() {
         int expected = 200;
         int actual = service.remain(2800);
-        assertEquals(expected, actual);
+        org.junit.Assert.assertEquals(expected, actual);
+    }
+
+    // JUnit5 tests
+    @org.junit.jupiter.api.Test
+    public void testJUnit5RemainWhenBelowBoundaryAmount() {
+        int expected = 200;
+        int actual = service.remain(800);
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testJUnit5RemainWhenBoundaryAmount() {
+        int expected = 0;
+        int actual = service.remain(1000);
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testJUnit5RemainWhenAboveBoundaryAmount() {
+        int expected = 200;
+        int actual = service.remain(2800);
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
     }
 }
